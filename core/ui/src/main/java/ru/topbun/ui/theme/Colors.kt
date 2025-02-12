99167224026de47fc66ea08a0ea83c926d6fa722
akimov1712 <akimov1712@mail.ru>
2025-02-12 14:29:19 +0300

    [chore]: 1. Create core modules (:core:ui, :core:common, :core:android); 2. Appended fonts in Fonts.kt 3. Appended typography in Typo.kt 4. Appended colors int Colors.kt

---
+++ core/ui/src/main/java/ru/topbun/ui/theme/Colors.kt
@@ -0,0 +1,29 @@
+package ru.topbun.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+object Colors {
+
+    val BG_100 = Color(0xFF1A1A1A)
+    val BG_200 = Color(0xFF292929)
+    val BG_300 = Color(0xFF404040)
+
+    val TEXT_100 = Color(0xFFFFFFFF)
+    val TEXT_200 = Color(0xFFA8A8A8)
+    val TEXT_300 = Color(0xFF222222)
+
+    val YELLOW_100 = Color(0xFFFFE34E)
+    val YELLOW_200 = Color(0xFFE3C421)
+
+    val RED_100 = Color(0xFFFF6347)
+    val RED_200 = Color(0xFFD7341D)
+    val RED_300 = Color(0xFF8D0000)
+
+    val GREEN_100 = Color(0xFF22F577)
+    val GREEN_200 = Color(0xFF31CE70)
+
+    val BLUE_100 = Color(0xFF2C32E0)
+    val BLUE_200 = Color(0xFF474EFF)
+
+}
