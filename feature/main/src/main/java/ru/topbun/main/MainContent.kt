87a1da3c37050f4a32d071f154bb8907759343d0
akimov1712 <akimov1712@mail.ru>
2025-02-15 15:39:35 +0300

    [feat]: 1. make vertical card 2. make vertical top card 3. make horizontal card 4. make film list

---
+++ feature/main/src/main/java/ru/topbun/main/MainContent.kt
@@ -0,0 +1,38 @@
+package ru.topbun.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import ru.topbun.main.components.Banner
+import ru.topbun.ui.theme.Colors
+
+object MainScreen: Screen {
+
+    @Composable
+    override fun Content() {
+        ScreenContent()
+    }
+
+}
+
+@Preview
+@Composable
+private fun ScreenContent(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Colors.BG_100),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Banner()
+
+    }
+}
+
+
